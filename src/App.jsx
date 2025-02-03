c539d142bdf3177a9fc6906800f75be9457d54b9
maldikurniawan <akurniawan2102@gmail.com>
2025-02-03 16:28:47 +0700

    fix responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,26 @@ import Logo from "/assets/images/logo-erp.png"
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen">
+    <div className="relative min-h-screen max-[500px]:px-4">
       <div
-        className="w-[299px] h-[299px] bg-[#20B9D4] absolute rounded-full blur-[200px] -z-10 top-0 left-0"
+        className="max-[500px]:w-[87px] max-[500px]:h-[87px] max-[500px]:blur-[75px] w-[299px] h-[299px] bg-[#20B9D4] absolute rounded-full blur-[200px] -z-10 top-0 left-0"
       ></div>
       <div
-        className="w-[299px] h-[299px] bg-[#20B9D4] absolute rounded-full blur-[200px] -z-10 top-0 right-0"
+        className="max-[500px]:w-[87px] max-[500px]:h-[87px] max-[500px]:blur-[75px] max-[500px]:top-[400px] w-[299px] h-[299px] bg-[#20B9D4] absolute rounded-full blur-[200px] -z-10 top-0 right-0"
       ></div>
       <div
-        className="w-[450px] h-[450px] bg-[#AC2477] absolute rounded-full blur-[200px] -z-10 bottom-0 left-0"
+        className="max-[500px]:w-[131px] max-[500px]:h-[131px] max-[500px]:blur-[75px] max-[500px]:bottom-[520px] w-[450px] h-[450px] bg-[#AC2477] absolute rounded-full blur-[200px] -z-10 bottom-0 left-0"
       ></div>
       <div
-        className="w-[450px] h-[450px] bg-[#AC2477] absolute rounded-full blur-[200px] -z-10 bottom-0 right-0"
+        className="max-[500px]:w-[131px] max-[500px]:h-[131px] max-[500px]:blur-[75px] max-[500px]:bottom-[150px] w-[450px] h-[450px] bg-[#AC2477] absolute rounded-full blur-[200px] -z-10 bottom-0 right-0"
       ></div>
       <div
-        className="w-[451px] h-[451px] bg-[#6BC364] absolute rounded-full blur-[250px] -z-10 top-0 inset-0 mx-auto"
+        className="max-[500px]:w-[131px] max-[500px]:h-[131px] max-[500px]:blur-[75px] w-[451px] h-[451px] bg-[#6BC364] absolute rounded-full blur-[250px] -z-10 top-0 inset-0 mx-auto"
       ></div>
       <div
-        className="w-[451px] h-[451px] bg-[#6BC364] absolute rounded-full blur-[250px] -z-10 bottom-0 left-1/2"
+        className="max-[500px]:hidden w-[451px] h-[451px] bg-[#6BC364] absolute rounded-full blur-[250px] -z-10 bottom-0 max-[500px]:left-0 left-1/2"
       ></div>
-      <div className="mx-auto w-[469px] py-[70px]">
+      <div className="mx-auto w-full sm:w-[469px] max-[500px]:py-4 py-[70px]">
         <Container icon={<img src={Logo} alt="ERP Logo" />} title={"ERP SKRIP"}>
           <Card>
             <div className="p-[10px]">
@@ -35,7 +35,7 @@ export default function Home() {
                 <span><img src="/assets/images/verified.svg" alt="Verified" className="mt-2" /></span>
               </div>
               <p className="text-[11px] text-[#808080]">Jln. Alam Flora no 41, Way Halim Permai, Kecamatan Way Halim, Kota Bandar Lampung, Lampung 351335</p>
-              <div className="flex justify-between mt-[26px]">
+              <a href="https://api.whatsapp.com/send?phone=6281927262596" target="_blank" className="flex justify-between mt-[26px]">
                 <div className="flex">
                   <div className="border p-[8px] rounded-[8px] w-[44px] h-[44px] flex justify-center mr-2">
                     <img src="/assets/images/phone.svg" alt="Phone" />
@@ -48,8 +48,8 @@ export default function Home() {
                 <div className="items-center my-auto">
                   <img src="/assets/images/arrow-right.svg" alt="Arrow Right" />
                 </div>
-              </div>
-              <div className="flex justify-between my-2 mb-4">
+              </a>
+              <a href="https://erpskrip.id/" target="_blank" className="flex justify-between my-2 mb-4">
                 <div className="flex">
                   <div className="border p-[8px] rounded-[8px] w-[44px] h-[44px] flex justify-center mr-2">
                     <img src="/assets/images/www.svg" alt="WWW" />
@@ -62,14 +62,14 @@ export default function Home() {
                 <div className="items-center my-auto">
                   <img src="/assets/images/arrow-right.svg" alt="Arrow Right" />
                 </div>
-              </div>
+              </a>
             </div>
           </Card>
           <Card>
-            <div className="py-[18px] px-[64px] text-[14px] text-[#1C1C1C] flex gap-2 justify-center text-center items-center mt-3">
+            <a href="https://api.whatsapp.com/send?phone=6281927262596" target="_blank" className="py-[18px] px-[64px] text-[14px] text-[#1C1C1C] flex gap-2 justify-center text-center items-center mt-3">
               <img src="/assets/images/whatsapp.svg" alt="Whats APP" />
               <span>What's App</span>
-            </div>
+            </a>
           </Card>
           <Card>
             <div className="py-[18px] px-[64px] text-[14px] text-[#1C1C1C] flex gap-2 justify-center text-center items-center mt-3">
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,13 +2,7 @@ import React from "react";
 
 const Container = ({ children, icon, title }) => {
     return (
-        <div
-            className="w-full p-[77px] rounded-[32px]"
-            style={{
-                boxShadow: `0px 4px 4px 0px rgba(0, 0, 0, 0.25)`,
-                background: `linear-gradient(163deg, rgba(255, 255, 255, 0.50) -1.51%, rgba(255, 255, 255, 0.80) 143.73%)`
-            }}
-        >
+        <div className="w-full max-[500px]:p-0 p-[77px] rounded-[32px] container-style">
             <div className="flex items-center gap-2 mb-4 font-medium text-black">
                 {icon && <div>{icon}</div>}
                 {title && <div className="text-md">{title}</div>}
