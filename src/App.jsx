33199c8f26fc43cfac820777f9f1142a03f99c1f
maldikurniawan <akurniawan2102@gmail.com>
2025-02-03 15:35:08 +0700

    add font family

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function Home() {
         className="w-[450px] h-[450px] bg-[#AC2477] absolute rounded-full blur-[200px] -z-10 bottom-0 right-0"
       ></div>
       <div
-        className="w-[451px] h-[451px] bg-[#6BC364] absolute rounded-full blur-[250px] -z-10 top-0 right-1/2"
+        className="w-[451px] h-[451px] bg-[#6BC364] absolute rounded-full blur-[250px] -z-10 top-0 inset-0 mx-auto"
       ></div>
       <div
         className="w-[451px] h-[451px] bg-[#6BC364] absolute rounded-full blur-[250px] -z-10 bottom-0 left-1/2"
@@ -34,17 +34,47 @@ export default function Home() {
                 <h1 className="uppercase font-bold text-[24px] mt-1">Supriyanto</h1>
                 <span><img src="/assets/images/verified.svg" alt="Verified" className="mt-2" /></span>
               </div>
-              <p className="text-[12px] text-[#808080]">Jln. Alam Flora no 41, Way Halim Permai, Kecamatan Way Halim, Kota Bandar Lampung, Lampung 351335</p>
+              <p className="text-[11px] text-[#808080]">Jln. Alam Flora no 41, Way Halim Permai, Kecamatan Way Halim, Kota Bandar Lampung, Lampung 351335</p>
+              <div className="flex justify-between mt-[26px]">
+                <div className="flex">
+                  <div className="border p-[8px] rounded-[8px] w-[44px] h-[44px] flex justify-center mr-2">
+                    <img src="/assets/images/phone.svg" alt="Phone" />
+                  </div>
+                  <div>
+                    <p className="text-[12px] text-[#525252]">Contact</p>
+                    <span className="text-black text-[14px] font-bold">+62 819-2726-2596</span>
+                  </div>
+                </div>
+                <div className="items-center my-auto">
+                  <img src="/assets/images/arrow-right.svg" alt="Arrow Right" />
+                </div>
+              </div>
+              <div className="flex justify-between my-2 mb-4">
+                <div className="flex">
+                  <div className="border p-[8px] rounded-[8px] w-[44px] h-[44px] flex justify-center mr-2">
+                    <img src="/assets/images/www.svg" alt="WWW" />
+                  </div>
+                  <div>
+                    <p className="text-[12px] text-[#525252]">Website</p>
+                    <span className="text-black text-[14px] font-bold">erpskrip.id</span>
+                  </div>
+                </div>
+                <div className="items-center my-auto">
+                  <img src="/assets/images/arrow-right.svg" alt="Arrow Right" />
+                </div>
+              </div>
             </div>
           </Card>
           <Card>
-            <div className="py-[18px] px-[64px] text-center items-center mt-3">
-              What's App
+            <div className="py-[18px] px-[64px] text-[14px] text-[#1C1C1C] flex gap-2 justify-center text-center items-center mt-3">
+              <img src="/assets/images/whatsapp.svg" alt="Whats APP" />
+              <span>What's App</span>
             </div>
           </Card>
           <Card>
-            <div className="py-[18px] px-[64px] text-center items-center mt-3">
-              G-Mail
+            <div className="py-[18px] px-[64px] text-[14px] text-[#1C1C1C] flex gap-2 justify-center text-center items-center mt-3">
+              <img src="/assets/images/gmail.svg" alt="G-Mail" />
+              <span>G-Mail</span>
             </div>
           </Card>
         </Container>
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const Container = ({ children, icon, title }) => {
     return (
-        <div className="bg-white/50 w-full p-[77px] rounded-[32px]" style={{ boxShadow: `0px 4px 4px 0px rgba(0, 0, 0, 0.25)` }}>
+        <div
+            className="w-full p-[77px] rounded-[32px]"
+            style={{
+                boxShadow: `0px 4px 4px 0px rgba(0, 0, 0, 0.25)`,
+                background: `linear-gradient(163deg, rgba(255, 255, 255, 0.50) -1.51%, rgba(255, 255, 255, 0.80) 143.73%)`
+            }}
+        >
             <div className="flex items-center gap-2 mb-4 font-medium text-black">
                 {icon && <div>{icon}</div>}
                 {title && <div className="text-md">{title}</div>}
